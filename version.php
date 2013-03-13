90d10df7668cec61a78241c0d86c16b3c0e523e2
Alex Rowe <arowe@studygroup.com>
2013-03-14 10:18:33 +1100

    Update version number and change require to Moodle 2.4

--- version.php
+++ version.php
@@ -14,8 +14,8 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-$plugin->version =  2012112901;
-$plugin->requires = 2012110900.00;
+$plugin->version =  2013031400;
+$plugin->requires = 2012120300.00;
 $plugin->cron     = 300;
 $plugin->component = 'plagiarism_turnitin';
 $plugin->maturity  = MATURITY_STABLE;
